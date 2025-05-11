bac9c1e90c9d4fa5faead16a3a4d891cc81c9a52
shai42 <1913sms@gmail.com>
2025-05-11 17:30:18 +0400

    Fixed  a Mistake in Naming

--- gui/__init__.py
+++ gui/__init__.py
@@ -3,7 +3,7 @@ Initialization file for the GUI package.
 """
 from .login import LoginWindow
 from .admin_dashboard import AdminDashboard
-from .tickets_gui import TicketBookingGUI
+from .ticket_gui import TicketsGUI
 from .reservation_gui import ReservationGUI
 
-__all__ = ['LoginWindow', 'AdminDashboard', 'TicketBookingGUI', 'ReservationGUI'] 
+__all__ = ['LoginWindow', 'AdminDashboard', 'TicketsGUI', 'ReservationGUI'] 
